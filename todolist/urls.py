652b7ff8989dbc6a3fe321b487ecb5153caef554
RafsanRifat <rafsan.rifat255@gmail.com>
2021-11-23 17:06:28 +0600

    logout views and urls added

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('base/', views.base, name='base'),
     path('login/', views.todo_login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.logout, name='logout'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.http import HttpResponse
 
 
@@ -26,6 +26,11 @@ def todo_login(request):
             return redirect('home')
     else:
         return render(request, 'login.html')
+
+
+def logout(request):
+    logout(request)
+    return redirect('login')
 
 def dashboard(request):
     return render(request, 'dashboard.html')
