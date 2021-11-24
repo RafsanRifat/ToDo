58cf052d5ebacd50401f57e24171679e975c583e
RafsanRifat <rafsan.rifat255@gmail.com>
2021-11-24 12:44:43 +0600

    UserCreationForm implemented in forms.py

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,2 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        fields = ('email', 'username', 'password1',)
