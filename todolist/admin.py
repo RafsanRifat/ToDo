200dba1c96d16afe46effd8ee8a66bc945cfc22d
RafsanRifat <rafsan.rifat255@gmail.com>
2021-11-25 15:58:07 +0600

    models created for collections and collection items

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from todolist.models import TodoItem
+from todolist.models import TodoItem, Collection, Collection_item
 
 # Register your models here.
 
 admin.site.register(TodoItem)
+admin.site.register(Collection)
+admin.site.register(Collection_item)
 
--- todolist/models.py
+++ todolist/models.py
@@ -9,3 +9,18 @@ class TodoItem(models.Model):
         return self.content
 
 
+
+class Collection(models.Model):
+    collection_name = models.TextField()
+
+    def __str__(self):
+        return self.collection_name
+
+
+class Collection_item(models.Model):
+    task = models.TextField()
+    collection_name = models.ForeignKey(Collection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task
+
