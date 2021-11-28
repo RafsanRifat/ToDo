699f43737523d1bd25eca68dae68ba1ee7b195cd
RafsanRifat <rafsan.rifat255@gmail.com>
2021-11-28 12:26:39 +0600

    model stracture changed

--- todolist/models.py
+++ todolist/models.py
@@ -5,19 +5,19 @@ from django.db import models
 
 
 
-class Task(models.Model):
-    task = models.TextField(null=True)
-    # collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.task
-        # return self.collection.collection_name
-
-
 class Collection(models.Model):
     collection_name = models.TextField(null=True)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True)
+    # task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True)
 
 
     def __str__(self):
         return self.collection_name
+
+
+class Task(models.Model):
+    task = models.TextField(null=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.task
+        # return self.collection.collection_name
