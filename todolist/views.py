d9f5eda97b4efe82b046744747df6f1754ad1106
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-02 17:47:10 +0600

    collection_list.html added

--- todolist/views.py
+++ todolist/views.py
@@ -39,8 +39,8 @@ def todo_logout(request):
 
 def dashboard(request):
     collection = Collection.objects.filter(user=request.user).all()  # show data only login users
-    # task = Task.objects.all()
-    context = {'collection': collection, 'task': task}
+    # count = Collection.
+    context = {'collection': collection, }
     return render(request, 'dashboard.html', context)
 
 
