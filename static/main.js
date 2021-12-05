236a16e1089fa14f280ddb4e045b572083832845
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-05 18:41:39 +0600

    user info working properly

--- static/main.js
+++ static/main.js
@@ -2,11 +2,23 @@ function goBack() {
     window.history.back();
 }
 
+// Collection edit
 function editFunction() {
     const x = document.getElementById("show-hide");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
+    }
+}
+
+
+// dashboard-login-info
+function infoFunction() {
+    const y = document.getElementById("user-info");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
     }
 }
