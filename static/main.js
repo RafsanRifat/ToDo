62642a187021530bfadd33e736ecae083fee5b84
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-05 17:12:20 +0600

    edit delete added

--- static/main.js
+++ static/main.js
@@ -1,3 +1,12 @@
 function goBack() {
-  window.history.back();
+    window.history.back();
+}
+
+function editFunction() {
+    const x = document.getElementById("show-hide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
