e0db65e57b577ed27e66679ef6bb206fb78012fb
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-08 12:04:57 +0600

    show working

--- static/main.js
+++ static/main.js
@@ -13,14 +13,24 @@ function editFunction() {
 }
 
 // Collection-item edit
-function itemEditFunction() {
-    const z = document.getElementById("show-hide-item");
-    if (z.style.display === "none") {
-        z.style.display = "block";
-    } else {
-        z.style.display = "none";
-    }
-}
+// function itemEditFunction() {
+//     const z = document.getElementById("show-hide-item");
+//     if (z.style.display === "none") {
+//         z.style.display = "block";
+//     } else {
+//         z.style.display = "none";
+//     }
+// }
+
+$(document).ready(function(){
+    // alert()
+    $('.collection_list_edit_button').on('click', function () {
+        // alert()
+        $('.collection_list_edit_button').parent().parent().find('.show-hide-item').hide();
+        $(this).parent().parent().find('.show-hide-item').show();
+    });
+});
+
 
 
 // dashboard-login-info
