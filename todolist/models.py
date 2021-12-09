f88a060a54387bd4e731956f4ee40c111841e67a
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-09 12:35:50 +0600

    some content changes in modals.html

--- todolist/models.py
+++ todolist/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 
 class Collection(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    collection_name = models.TextField(null=True)
+    collection_name = models.CharField(null=True, max_length=500)
 
     def __str__(self):
         return self.collection_name
 
 
 class Task(models.Model):
-    task = models.TextField(null=True)
+    task = models.CharField(null=True, max_length=500)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
